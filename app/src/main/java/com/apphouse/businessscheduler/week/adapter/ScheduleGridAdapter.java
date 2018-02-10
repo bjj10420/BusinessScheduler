c0ac7defa3a2b3ae47f2d44a0430f2c4a473883b
bjj10420 <bjj10420@hotmail.com>
2018-02-10 14:39:40 +0900

    그리드뷰 랜덤으로 숫자배치되는 현상 해결 convert 재활용을 빼줌

--- app/src/main/java/com/apphouse/businessscheduler/week/adapter/ScheduleGridAdapter.java
+++ app/src/main/java/com/apphouse/businessscheduler/week/adapter/ScheduleGridAdapter.java
@@ -1,6 +1,9 @@
 package com.apphouse.businessscheduler.week.adapter;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +12,7 @@ import android.widget.TextView;
 
 import com.apphouse.businessscheduler.R;
 
-public class ScheduleGridAdapter extends BaseAdapter{
+public class ScheduleGridAdapter extends BaseAdapter {
 
     LayoutInflater li;
 
@@ -33,10 +36,31 @@ public class ScheduleGridAdapter extends BaseAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
-        if(convertView == null)
-            convertView = li.inflate(R.layout.schedule_grid_item, null);
-        ((TextView)convertView.findViewById(R.id.gridItemText)).setText(String.valueOf(position));
-        return convertView;
+    public View getView(int position, View gridItemView, ViewGroup viewGroup) {
+
+        gridItemView = li.inflate(R.layout.schedule_grid_item, null);
+        gridItemView.setTag(position);
+        setConvertViewContent(gridItemView, position);
+        return gridItemView;
     }
+
+    private void setConvertViewContent(View convertView, int position) {
+        if (isTimePanel(convertView))
+            setTimePanelView(convertView);
+
+        setContentText(convertView, position);
+    }
+
+    private void setContentText(View convertView, int position) {
+        ((TextView) convertView.findViewById(R.id.gridItemText)).setText(String.valueOf(position));
+    }
+
+    private boolean isTimePanel(View convertView) {
+        return (Integer) convertView.getTag() % 9 == 0;
+    }
+
+    private void setTimePanelView(View convertView) {
+        convertView.setBackgroundColor(Color.parseColor("#ffffff"));
+    }
+
 }
